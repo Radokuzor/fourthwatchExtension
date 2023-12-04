c450a0e4357ba74fac94a0bf5a7314c7ab8e6691
Radokuzor <102080314+Radokuzor@users.noreply.github.com>
2023-12-03 23:34:00 -0600

    chat works

--- background.js
+++ background.js
@@ -12,7 +12,35 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   }
 });
 
-function saveText(text) {
-  savedTexts.push(text);
+async function saveText(text) {
+
+  // send to chat 
+  const apiKey = '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';
+  const apiUrl = 'https://api.openai.com/v1/chat/completions';
+
+  const response = await fetch(apiUrl, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${apiKey}`,
+    },
+    body: JSON.stringify({
+      prompt: text,
+      max_tokens: 50,  // Adjust as needed
+    }),
+  });
+
+  const data = await response.json();
+  console.log(`HTTP error! Status: ${response.status}`)
+  const chatGptResponse = data.choices[0]?.text || 'No response from ChatGPT';
+
+  // Optionally, you can do something with the ChatGPT response, such as log it or display it.
+  console.log('ChatGPT Response:', chatGptResponse);
+
+  // Notify the popup to update with the latest saved texts
+
+  savedTexts.push(chatGptResponse);
   chrome.runtime.sendMessage({ action: "updatePopup", texts: savedTexts });
+
+  // chrome.runtime.sendMessage({ action: "updatePopup", texts: savedTexts });
 }
--- content.js
+++ content.js
@@ -1,6 +1,6 @@
 chrome.runtime.sendMessage({ action: "checkText" });
 
-document.addEventListener("mouseup", (event) => {
+document.addEventListener("mouseup", async (event) => {
   const selectedText = window.getSelection().toString().trim();
   if (selectedText !== "") {
     chrome.runtime.sendMessage({ action: "saveText", text: selectedText });
